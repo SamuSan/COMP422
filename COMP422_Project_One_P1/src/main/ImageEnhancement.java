89c45fe90a90589ddd72dc3377a73c708f5e217f
SamuSan <sambminns@gmail.com>
2014-08-02 23:23:23 +1200

    Trying to reconfigure this repo, might be alost cause

--- COMP422_Project_One_P1/src/main/ImageEnhancement.java
+++ COMP422_Project_One_P1/src/main/ImageEnhancement.java
@@ -10,14 +10,14 @@ import javax.imageio.ImageIO;
 
 public class ImageEnhancement {
 
-//	private int[][] enhanceMask = { { -1, -1, -1 }, 
-//									{ -1, 8, -1 }, 
-//									{ -1, -1, -1 } };
-	
-	private int[][] enhanceMask = { { 0, 0, 0 }, 
-									{0, 1, 0 }, 
-									{ 0, 0, 0 } };
-	
+//	 private int[][] enhanceMask = { { -1, -1, -1 },
+//	 { -1, 8, -1 },
+//	 { -1, -1, -1 } };
+
+//	private int[][] enhanceMask = { { 0, -1, 0 }, { -1, 5, -1 }, { 0, -1, 0 } };
+//	private int[][] enhanceMask = { { 1, -2, -1 }, { -2, 5, -2 }, { 1, -2, -1 } };
+	private int[][] enhanceMask = { { -1, -1, -1 }, { -1, 9, -1 }, { -1, -1, -1 } };
+
 	private int width = 0;
 	private int height = 0;
 	private BufferedImage src;
@@ -39,7 +39,7 @@ public class ImageEnhancement {
 		System.out.println(src.getWidth());
 		int s1 = 0;
 		int s2 = 0;
-		int pixVal =0;
+		int pixVal = 0;
 		tempImg = new BufferedImage(width + 2, height + 2, src.getType());
 		int[] srcData = src.getRGB(0, 0, width, height, null, 0, width);
 		Color temp = new Color(255, 255, 255);
@@ -53,39 +53,51 @@ public class ImageEnhancement {
 		for (int i = 1; i < tempImg.getWidth() - 2; i++) {
 			for (int j = 1; j < tempImg.getHeight() - 2; j++) {
 				int rgb = src.getRGB(i, j);
-//				System.out.println(rgb);
+				// System.out.println(rgb);
 				tempImg.setRGB(i, j, src.getRGB(i, j));
 			}
 		}
 
-		 FileConverter f = new FileConverter();
-		 f.writeOut(tempImg, "outputTest");
+		FileConverter f = new FileConverter();
+		f.writeOut(tempImg, "outputTest");
 
-		 for (int i = 0; i < enhanceMask.length; i++) {
-			for (int j = 0; j < enhanceMask[0].length; j++) {
-				System.out.printf("%d",enhanceMask[i][j]);
-			}
-			System.out.println();
-		}
-		 
-out = new BufferedImage(tempImg.getWidth(), tempImg.getHeight(), tempImg.getType());
-		for (int i = 1; i < tempImg.getWidth()-1; i++) {
-			for (int j = 1; j < tempImg.getHeight()-1; j++) {
-				pixVal += enhanceMask[0][0] * tempImg.getRGB(i - 1, j - 1);// topleft
-						
+//		for (int i = 0; i < enhanceMask.length; i++) {
+//			for (int j = 0; j < enhanceMask[0].length; j++) {
+//
+//				System.out.printf("%d  i: %d j :  %d\n", enhanceMask[i][j], i,
+//						j);
+//			}
+//
+//			System.out.println();
+//		}
+
+		out = new BufferedImage(tempImg.getWidth(), tempImg.getHeight(),
+				tempImg.getType());
+		for (int i = 1; i < tempImg.getWidth() - 1; i++) {
+			for (int j = 1; j < tempImg.getHeight() - 1; j++) {
 				
-				
-//				System.out.println("S1: "+s1+":: S2: "+ s2);
-//				pixVal = (int )Math.sqrt(Math.pow(s1, 2) + Math.pow(s2, 2));
+//				System.out.println("Original pixVal at start : "  + pixVal);
+				pixVal += enhanceMask[0][0] * tempImg.getRGB(i - 1, j - 1);
+				pixVal += enhanceMask[0][1] * tempImg.getRGB(i - 1, j);
+				pixVal += enhanceMask[0][2] * tempImg.getRGB(i - 1, j + 1);
+				pixVal += enhanceMask[1][0] * tempImg.getRGB(i, j - 1);
+				pixVal += enhanceMask[1][1] * tempImg.getRGB(i, j);
+				pixVal += enhanceMask[1][2] * tempImg.getRGB(i, j + 1);
+				pixVal += enhanceMask[2][0] * tempImg.getRGB(i + 1, j);
+				pixVal += enhanceMask[2][1] * tempImg.getRGB(i + 1, j - 1);
+				pixVal += enhanceMask[2][2] * tempImg.getRGB(i + 1, j + 1);
+//System.out.println("Pixvalue " + pixVal );
+				// System.out.println("S1: "+s1+":: S2: "+ s2);
+				// pixVal = (int )Math.sqrt(Math.pow(s1, 2) + Math.pow(s2, 2));
 				out.setRGB(i, j, pixVal);
-//				System.out.println("Pix Value: " + pixVal);
+				pixVal = 0;
+				// System.out.println("Pix Value: " + pixVal);
 
 			}
-			pixVal =0;
+		
 		}
 
-		 f.writeOut(out, "outputEnhanced");
+		f.writeOut(out, "outputEnhanced2");
 	}
 
-		
 }
