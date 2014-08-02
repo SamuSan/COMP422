2664c78fb146ceb67f487d3aea4d92c6cefa18bc
SamuSan <sambminns@gmail.com>
2014-08-02 20:30:20 +1200

    Starting enhancement

--- COMP422_Project_One_P1/src/main/NoiseCancellation.java
+++ COMP422_Project_One_P1/src/main/NoiseCancellation.java
@@ -15,7 +15,6 @@ public class NoiseCancellation {
 	private int width = 0;
 	private int height = 0;
 	private BufferedImage src;
-	private BufferedImage tempImg;
 	private BufferedImage out;
 
 	public NoiseCancellation(BufferedImage in) {
@@ -29,13 +28,10 @@ public class NoiseCancellation {
 	}
 
 	public void performMedianNoiseCancellation() {
-		System.out.println("Hiehgt "+src.getHeight());
-		System.out.println("Width "+src.getWidth());
 		int pixVal = 0;
-		out = new BufferedImage(src.getWidth(), src.getHeight(),
-				src.getType());
-		for (int j = 1; j < out.getWidth()-1; j++) {
-			for (int i = 1; i < out.getHeight()-1; i++) {
+		out = new BufferedImage(src.getWidth(), src.getHeight(), src.getType());
+		for (int j = 1; j < out.getWidth() - 1; j++) {
+			for (int i = 1; i < out.getHeight() - 1; i++) {
 				ArrayList<Integer> vals = new ArrayList<Integer>();
 
 				vals.add(src.getRGB(j - 1, i - 1));// topleft
@@ -50,24 +46,18 @@ public class NoiseCancellation {
 
 				Collections.sort(vals);
 				pixVal = vals.get(5);
-//				System.out.println(i  + " : " + j);
 				out.setRGB(j, i, pixVal);
-
 			}
 		}
-//
 		FileConverter f = new FileConverter();
 		f.writeOut(out, "outputNoise");
 	}
-	
-	public void performGaussianNoiseCancellation() {
-		System.out.println("Hiehgt "+src.getHeight());
-		System.out.println("Width "+src.getWidth());
+
+	public void performMedianNoiseWithTweakCancellation() {
 		int pixVal = 0;
-		out = new BufferedImage(src.getWidth(), src.getHeight(),
-				src.getType());
-		for (int j = 1; j < out.getWidth()-1; j++) {
-			for (int i = 1; i < out.getHeight()-1; i++) {
+		out = new BufferedImage(src.getWidth(), src.getHeight(), src.getType());
+		for (int j = 1; j < out.getWidth() - 1; j++) {
+			for (int i = 1; i < out.getHeight() - 1; i++) {
 				ArrayList<Integer> vals = new ArrayList<Integer>();
 
 				vals.add(src.getRGB(j - 1, i - 1));// topleft
@@ -82,12 +72,50 @@ public class NoiseCancellation {
 
 				Collections.sort(vals);
 				pixVal = vals.get(5);
-//				System.out.println(i  + " : " + j);
+				out.setRGB(j, i, pixVal);
+			}
+		}
+
+		for (int j = 1; j < out.getWidth() - 1; j++) {
+			for (int i = 1; i < out.getHeight() - 1; i++) {
+				int pixval = 0;
+
+				src.getRGB(j, i);// midmid
+
+				out.setRGB(j, i, pixVal);
+			}
+		}
+		FileConverter f = new FileConverter();
+		f.writeOut(out, "outputNoiseTweaked");
+	}
+
+	public void performGaussianNoiseCancellation() {
+		System.out.println("Hiehgt " + src.getHeight());
+		System.out.println("Width " + src.getWidth());
+		int pixVal = 0;
+		out = new BufferedImage(src.getWidth(), src.getHeight(), src.getType());
+		for (int j = 1; j < out.getWidth() - 1; j++) {
+			for (int i = 1; i < out.getHeight() - 1; i++) {
+				ArrayList<Integer> vals = new ArrayList<Integer>();
+
+				vals.add(src.getRGB(j - 1, i - 1));// topleft
+				vals.add(src.getRGB(j - 1, i));// topmid
+				vals.add(src.getRGB(j - 1, i + 1));// topright
+				vals.add(src.getRGB(j, i - 1));// midleft
+				vals.add(src.getRGB(j, i));// midmid
+				vals.add(src.getRGB(j, i + 1));// midright
+				vals.add(src.getRGB(j + 1, i - 1));// botleft
+				vals.add(src.getRGB(j + 1, i));// botmid
+				vals.add(src.getRGB(j + 1, i + 1));// botright
+
+				Collections.sort(vals);
+				pixVal = vals.get(5);
+				// System.out.println(i + " : " + j);
 				out.setRGB(j, i, pixVal);
 
 			}
 		}
-//
+		//
 		FileConverter f = new FileConverter();
 		f.writeOut(out, "outputNoise");
 	}
