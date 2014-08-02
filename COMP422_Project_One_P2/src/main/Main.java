7dfed97a1b0e3800f4e57f6442f6d0e5bce9461a
SamuSan <sambminns@gmail.com>
2014-08-02 23:34:31 +1200

    Space Mining started

--- COMP422_Project_One_P2/src/main/Main.java
+++ COMP422_Project_One_P2/src/main/Main.java
@@ -1,9 +1,38 @@
 package main;
 
 public class Main {
-
+private static int oper=-1;
+private static FileConverter f = new FileConverter();
 	public static void main(String [] args){
-		
+		if (args.length > 1) {
+			System.out
+					.println("Incorrect Number of Arguments: Please enter '1','2' or '3'");
+		} else {
+			oper = Integer.parseInt(args[0]);
+		}
+
+		switch (oper) {
+		case 1:
+			System.out.println("Mining The Spaces");
+			doSpaceMining();
+			System.out.println("Done");
+			break;
+		case 2:
+			System.out.println("Noise Cancellation selected");
+			System.out.println("Done");
+			break;
+		case 3:
+			System.out.println("Image Enhancement selected");
+			System.out.println("Done");
+			break;
+
+		default:
+			break;
+		}
 	}
 
+	private static void doSpaceMining(){
+		SpaceMining sp = new SpaceMining(f.convert("hubble.tif"));
+	}
+	
 }
---
+++ COMP422_Project_One_P2/src/main/SpaceMining.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.awt.image.BufferedImage;
+
+public class SpaceMining {
+BufferedImage src = null;
+	public SpaceMining(BufferedImage in) {
+		src =in;
+	}
+
+}
